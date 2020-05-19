8435290674d85e0f437e9cf84a94f5c1d74f3141
s.lanzini <s.lanzini@almaviva.it>
2020-05-19 10:38:31 +0200

    first commit

---
+++ src/test/java/it/lanzini/simulationapp/SimulationappApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lanzini.simulationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulationappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
